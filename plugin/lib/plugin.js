c20ef4360d6f2bf938bafd131bf23a88891042cb
Adam Hirst <adam@ahirst.com>
2017-03-04 18:00:46 +0000

    Added menu items

--- plugin/lib/plugin.js
+++ plugin/lib/plugin.js
@@ -1,49 +1,71 @@
 'use babel';
 
-import PluginView from './plugin-view';
 import { CompositeDisposable } from 'atom';
 
 export default {
 
-  pluginView: null,
-  modalPanel: null,
   subscriptions: null,
+  socket: null,
 
   activate(state) {
-    this.pluginView = new PluginView(state.pluginViewState);
-    this.modalPanel = atom.workspace.addModalPanel({
-      item: this.pluginView.getElement(),
-      visible: false
-    });
-
     // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
     this.subscriptions = new CompositeDisposable();
 
     // Register command that toggles this view
     this.subscriptions.add(atom.commands.add('atom-workspace', {
-      'plugin:toggle': () => this.toggle()
+      'plugin:host': () => this.hostInstance()
     }));
+    // this.subscriptions.add(atom.commands.add('atom-workspace', {
+    //   'plugin:stop': () => this.stopHosting()
+    // }));
   },
 
   deactivate() {
-    this.modalPanel.destroy();
     this.subscriptions.dispose();
-    this.pluginView.destroy();
   },
 
-  serialize() {
-    return {
-      pluginViewState: this.pluginView.serialize()
-    };
+  serialize() {},
+
+  hostInstance() {
+
+      var data = {
+          username: "Bob",
+          files: {
+              test: "This is a test document"
+          }
+      };
+
+      var socket = require('socket.io-client')("http://localhost:8000/socket.io/");
+      socket.on('update', function(d) { console.log(d); });
+
+      socket.connect();
+
+      socket.emit('register', data);
+
+      socket.close();
+
+      var code = "YXKSM";
+      atom.notifications.addSuccess("Success! Paste this code to collaborators: " + code,
+        {
+            dismissable: true,
+            buttons: [
+                {
+                    text: "Copy",
+                    onDidClick: function() {
+                        atom.clipboard.write(code);
+                    }
+                },
+                {
+                    text: "Stop hosting",
+                    onDidClick: function() {
+                        stopHosting();
+                    }
+                }
+            ]
+        });
   },
 
-  toggle() {
-    console.log('Plugin was toggled!');
-    return (
-      this.modalPanel.isVisible() ?
-      this.modalPanel.hide() :
-      this.modalPanel.show()
-    );
-  }
+  stopHosting() {
+  },
 
 };
