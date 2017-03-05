8723ff3ec86de86a64e5166f80bdac0b375b279c
samwalls <sam@s-w.io>
2017-03-05 00:03:27 +0000

    superior quoting

--- server/main.js
+++ server/main.js
@@ -1,14 +1,14 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('register', function() {
-  	console.log('register recieved');
+io.on("connection", function(socket){
+  console.log("a user connected");
+  socket.on("register", function() {
+  	console.log("register recieved");
   });
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
